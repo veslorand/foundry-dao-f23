10518e5186d3d5e041807f0a5e86a50d0200f611
veslorand <veslorand@gmail.com>
2024-05-19 19:20:27 +0200

    Add OpenZeppelin contracts as a dependency

---
+++ src/Box.sol
@@ -0,0 +1,31 @@
+// contracts/Box.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable {
+    constructor() Ownable(msg.sender) {
+    }
+
+    uint256 private s_number;
+
+    // Emitted when the stored value changes
+    event ValueChanged(uint256 newNumber);
+
+    // Stores a new value in the contract
+    function store(uint256 newNumber) public onlyOwner {
+        s_number = newNumber;
+        emit ValueChanged(newNumber);
+    }
+
+    // Reads the last stored value
+    function getNumber() public view returns (uint256) {
+        return s_number;
+    }
+
+    // Reads the last stored value
+    function retrieve() public view returns (uint256) {
+        return s_number;
+    }
+}
